be1f25a90dc357b96c937e69fc634ad48a16aff1
Yallamanda Madhulatha <madhulathayallamanda@gmail.com>
2026-06-21 18:57:39 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -818,4 +818,8 @@ def static_files(path):
 load_data()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=False)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
+        debug=False,
+    )
